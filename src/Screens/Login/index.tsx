f17d0219dc1bc0c6aa0fe9abd206ce0a2d0be9dc
Omer Aayan <umerayan6@gmail.com>
2024-06-03 09:07:17 +0500

    Removing "back" from welcome

--- src/Screens/Login/index.tsx
+++ src/Screens/Login/index.tsx
@@ -49,7 +49,7 @@ const Login: React.FC = () => {
   return (
     <div className="login-container">
       <FormCard
-        title="Welcome Back"
+        title="Welcome"
         inputFields={[
           {
             label: "Email",
