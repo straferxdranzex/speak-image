383711fe67bef2875db3953e9276695db8d305e6
Omer Aayan <umerayan6@gmail.com>
2024-07-09 00:57:03 +0500

    Update index.tsx

--- src/Screens/Cover/index.tsx
+++ src/Screens/Cover/index.tsx
@@ -80,9 +80,9 @@ const Cover: React.FC = () => {
             animate={{ opacity: 1 }}
             transition={{ delay: 2, duration: 3, ease: "easeOut" }}
           >
-            Supercharge your creativity and
+            Supercharge and stimulate your knowledge with
             <br />
-            productivity <span className="cover-title-colored">visually</span>
+            images <span className="cover-title-colored">visually</span>
           </motion.span>
           <button className="cover-button" onClick={handleGetStartedClick}>
             Get Started
